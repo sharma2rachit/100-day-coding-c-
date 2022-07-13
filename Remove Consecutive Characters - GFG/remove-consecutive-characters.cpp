b63c4f9d5b8d56ec6b3dfb56413a3d455054273e
Rachit Sharma <73378516+sharma2rachit@users.noreply.github.com>
2022-07-13 20:30:46 +0530

    Added solution - LeetHub

--- Remove Consecutive Characters - GFG/remove-consecutive-characters.cpp
+++ Remove Consecutive Characters - GFG/remove-consecutive-characters.cpp
@@ -11,10 +11,8 @@ class Solution{
         // code here.
         string ans;
         for(int i=0;i<S.length();i++){
-            if(S[i]!=S[i-1]){
-                ans+=S[i];
-            }
-            
+            if(S[i]==S[i-1]) continue;
+            else ans+=S[i];
         }
         return ans;
     }
